59b938280f725f76ab83c2d7cfd1a70917a907dd
Abhinaykrishna2 <114848522+Abhinaykrishna2@users.noreply.github.com>
2024-02-03 09:45:10 +0530

    websocketready

--- app.py
+++ app.py
@@ -1,41 +1,135 @@
 import asyncio
+import json
 import websockets
 
-global_data = {
-    'InitialRequest': ''
-}  # for data preservance
-
-flags = {}  # for data communication to js websocket
 
 async def handle_connection(websocket, path):
-    try:
-        async for message in websocket:
-            print(f"Received message from client: {message}")
+    print('Connected to JS')
 
-            if message.lower() == 'medical prescription':
-                global_data['InitialRequest'] = 'medical prescription'
-                msg = 'Sure, May I know what are those medical issues troubling you'
+    while True:
+        data = await websocket.recv()
+        if not data:
+            break  
 
-                flags = {
-                    'msg': msg,
-                    'nextEvent': 'prescribeMedicine',  # function with the same name to be called next
-                    'option': []
-                }
-                await websocket.send(str(flags))
-    except websockets.exceptions.ConnectionClosedError:
-        print("Connection closed")
+        re=data.split('==')
+        print()
+        await process_data(data, websocket)
 
-async def start_server():
-    server = await websockets.serve(handle_connection, "localhost", 8005)
-    print("Server started on ws://localhost:8005")
+async def process_data(websocket):
+    data = await websocket.recv()
+    print(data)
+    re=data.split('==')
+    if re[0].lower() == 'medical prescription':
+        response={
+            "msg" : "May I kindly request a brief description of your current medical condition?",
+            "option" : [],
+        }
+        await send_response(response, websocket)
+    elif re[0].lower() == 'depression test':
+        response={
+            "msg" : "I had trouble relaxing and calming down.",
+            "option" : ["1- Did not apply to me at all",
+                        "2- Applied to me to some degree, or some of the time",
+                        "3- Applied to me to a considerable degree or a good part of time",
+                        "4- Applied to me very much or most of the time"],
+            "nextEvent" : 'q1',
+            "cnt" : 0
+        }
+        await send_response(response, websocket)
+        user_response = await asyncio.wait_for(websocket.recv(), timeout=10)
+        print('++++++++++++++',user_response)
+        temp=user_response.split('==')
+        print("{}('{}', websocket)".format(temp[1], user_response))
+        await eval("{}('{}', websocket)".format(temp[1], user_response))
+    else:
+        await default_response(websocket)
 
-    try:
-        await server.wait_closed()
-    except asyncio.CancelledError:
-        pass
+async def q1(data, websocket):
+    print(data)
+    pcab=data.split('==')
+    req=pcab[0].split('-')
+    response={
+            "msg" : "I was aware of dryness of my mouth.",
+            "option" : ["1- Did not apply to me at all",
+                        "2- Applied to me to some degree, or some of the time",
+                        "3- Applied to me to a considerable degree or a good part of time",
+                        "4- Applied to me very much or most of the time"],
+            "nextEvent" : 'q2',
+            "cnt" : int(req[0])
+
+        }
+    print('=====================','Reached here')
+    await send_response(response, websocket)
+    user_response = await asyncio.wait_for(websocket.recv(), timeout=10)
+    print('++++++++++++++',user_response)
+    temp=user_response.split('==')
+    print("{}('{}', websocket)".format(temp[1], user_response))
+    await eval("{}('{}', websocket)".format(temp[1], user_response))
+
+async def q2(data, websocket):
+    print(data)
+    pcab=data.split('==')
+    req=pcab[0].split('-')
+    response={
+            "msg" : "I couldn’t seem to experience any positive feeling at all",
+            "option" : ["1- Did not apply to me at all",
+                        "2- Applied to me to some degree, or some of the time",
+                        "3- Applied to me to a considerable degree or a good part of time",
+                        "4- Applied to me very much or most of the time"],
+            "nextEvent" : 'q3',
+            "cnt" : int(req[0])
+
+        }
+    print('=====================','Reached here')
+    await send_response(response, websocket)
+    user_response = await asyncio.wait_for(websocket.recv(), timeout=10)
+    print('++++++++++++++',user_response)
+    temp=user_response.split('==')
+    print("{}('{}', websocket)".format(temp[1], user_response))
+    await eval("{}('{}', websocket)".format(temp[1], user_response))
+
+async def q3(data, websocket):
+    print(data)
+    pcab=data.split('==')
+    req=pcab[0].split('-')
+    response={
+            "msg" : "I experienced breathing difficulty (e.g. excessively rapid breathing, breathlessness in the absence of physical exertion) ",
+            "option" : ["1- Did not apply to me at all",
+                        "2- Applied to me to some degree, or some of the time",
+                        "3- Applied to me to a considerable degree or a good part of time",
+                        "4- Applied to me very much or most of the time"],
+            "nextEvent" : 'q2',
+            "cnt" : int(req[0])
+
+        }
+    print('=====================','Reached here')
+    await send_response(response, websocket)
+    user_response = await asyncio.wait_for(websocket.recv(), timeout=10)
+    print('++++++++++++++',user_response)
+    temp=user_response.split('==')
+    print("{}('{}', websocket)".format(temp[1], user_response))
+    await eval("{}('{}', websocket)".format(temp[1], user_response))
+
+
+
+
+
+async def default_response(websocket):
+    print('Default response')
+    response = {"msg": "I didn't understand that. Please choose a valid option.", "option": []}
+    await send_response(response, websocket)
+
+async def send_response(response, websocket):
+    response_json = json.dumps(response)
+    await websocket.send(response_json)
 
 async def main():
-    await start_server()
+    server = await websockets.serve(
+        process_data, '0.0.0.0', 8005
+    )
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    async with server:
+        await server.wait_closed()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
--- chatscript.js
+++ chatscript.js
@@ -1,42 +1,108 @@
+const socket = new WebSocket("ws://127.0.0.1:8005");
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Python");
+});
+
+var cnt=0;
+
 function generateOptionButtons(options) {
     var optionsContainer = document.getElementById('options-container');
-    if(options.length!=0) {
-    optionsContainer.innerHTML = "<p>Choose an option:</p>";
-    }
+    console.log('#####################'+options);
 
-    for (var i = 0; i < options.length; i++) {
-        var button = document.createElement('button');
-        button.textContent = options[i];
-        button.onclick = function() {
-            selectOption(this.textContent);
-        };
-        optionsContainer.appendChild(button);
+    optionsContainer.innerHTML = '';
+
+    if(options.length !== 0) {
+        var messageParagraph = document.createElement('p');
+        messageParagraph.textContent = "Choose an option:";
+        optionsContainer.appendChild(messageParagraph);
+
+        for (var i = 0; i < options.length; i++) {
+            var button = document.createElement('button');
+            button.textContent = options[i];
+            button.onclick = function() {
+                selectOption(this.textContent);
+            };
+            optionsContainer.appendChild(button);
+        }
     }
 }
+
+
 function selectOption(optionText) {
     document.getElementById('user-input').value = optionText;
 }
+
 var exampleOptions = ['Medical Prescription', 'Depression Test'];
 generateOptionButtons(exampleOptions);
 
-const socket = new WebSocket("ws://localhost:8005");
+socket.addEventListener("message", (event) => {
+    console.log('Breakpoint')
+    const response = JSON.parse(event.data);
+    console.log("Received response from Python:", response);
+    updatedUI(response);
+});
 
-function pyconnect(data){
-    
-    socket.addEventListener("open", () => {
-        console.log("Connected to Python");
-        socket.send(data);
-    });
-    var response;
-    socket.addEventListener("message", (event) => {
-        response = event.data;
-        console.log(`Received message from Python: ${response}`);
-    });
-    return response;
+function pyconnect(data) {
+    socket.send(data);
 }
+var nextEvent = ''
+var bool=true;
 
 function sendMessage() {
     var data = document.getElementById('user-input').value;
-    console.log('Data being sent')
-    pyconnect(data)
+    console.log(nextEvent);
+
+    var buttonClickPromise = new Promise(function(resolve, reject) {
+        document.getElementById('user-input').addEventListener('input', function() {
+            resolve();
+        });
+    });
+
+    if (exampleOptions[0] == 'Medical Prescription') {
+        data += '==' + 'process_data'
+    } else {
+        
+        buttonClickPromise.then(function() {
+            console.log('Button clicked');
+            data=document.getElementById('user-input').value;            
+        });
+        console.log('#####');
+        console.log(data);
+        console.log(nextEvent);
+        console.log('#####');
+        data += '==' + nextEvent;
+    }
+
+    console.log(data);
+    console.log('Data being sent');
+    if(bool)
+    {
+        pyconnect(data);
+        bool=false;
+    }
+    else{
+        bool=true;
+    }
+    
+    document.getElementById('user-input').value = '';
+}
+
+
+function updatedUI(response) {
+    console.log('Printing msg :'+response.msg);
+    console.log('Printing options : '+response.option)
+    
+    document.getElementById('chat-body').innerText = response.msg;
+    exampleOptions = response.option;
+    generateOptionButtons(exampleOptions);
+    document.getElementById('user-input').value = '';
+
+    if(response.cnt)
+    {
+        cnt+=response.cnt;
+        console.log('++++++++++++'+cnt+'+++++++++++++++++');
+    }
+    nextEvent=response.nextEvent;
+    sendMessage();
 }
